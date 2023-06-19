a26ef58d8eabc9afb19f6cf55abd10e816b61ead
Gunay HUmmatova <hummatovagunay02@gmail.com>
2023-06-19 15:36:01 +0400

    user can send review and stars now

--- Clinique-clone-web-site/js/useraccount.js
+++ Clinique-clone-web-site/js/useraccount.js
@@ -38,15 +38,6 @@ function fillBasket(arr){
 }
 let customer= JSON.parse(localStorage.getItem('normalUser'))
 
-// async function getBasketItems(){
-//     const res= await axios(CUSTOMER_API)
-//     const data= await res.data
-//     let activeCustomer=data.find(item=>item.customerEmail===customer.userEmail)
-//     badge.innerHTML=activeCustomer.basketItems.length
-//     fillBasket(activeCustomer.basketItems)
-// }
-// getBasketItems()
-
 let totalPrice=document.querySelector('.total-price')
  async function calculatePrice(){
   const res= await axios(CUSTOMER_API)
@@ -92,7 +83,51 @@ function fillCustomerOrders(arr){
       allOrders.innerHTML+=`
       <div class="orderedProd">
       <img src="./images/${item.productImageMain}" alt="">
-      <span class="prodName">${item.productName}</span>
+      <span class="prodName">Almost Lipstick in Black Honey</span>
+<button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#${item.id}">
+How was your experience?
+</button>
+<div class="modal fade review-modal" id="${item.id}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+<div class="modal-dialog modal-dialog-centered">
+<div class="modal-content">
+<div class="modal-header">
+<h1 class="modal-title fs-5" id="staticBackdropLabel"></h1>
+<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+</div>
+<div class="modal-body">
+<form action="" onsubmit="sendReview(${item.id})">
+<textarea name="" id="userReview" cols="30" rows="10" required placeholder="Share your thoughts with us" oninput="sendThought(this)"></textarea> 
+<div class="rating">
+<input value="star-1" required name="star-radio" id="star-1" type="radio" onclick="rate(5)" >
+<label for="star-1">
+  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" pathLength="360"></path></svg>
+</label>
+<input value="star-1" required name="star-radio" id="star-2" type="radio" onclick="rate(4, ${item.id})" >
+<label for="star-2">
+  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" pathLength="360"></path></svg>
+</label>
+<input value="star-1" required name="star-radio" id="star-3" type="radio" onclick="rate(3, ${item.id})" >
+<label for="star-3">
+  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" pathLength="360"></path></svg>
+</label>
+<input value="star-1" required name="star-radio" id="star-4" type="radio" onclick="rate(2, ${item.id})" >
+<label for="star-4">
+  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" pathLength="360"></path></svg>
+</label>
+<input value="star-1" required name="star-radio" id="star-5" type="radio" onclick="rate(1, ${item.id})" >
+<label for="star-5">
+  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" pathLength="360"></path></svg>
+</label>
+</div> 
+<button type="submit" class="sendReview mt-3">Send</button>
+</form>
+</div>
+<div class="modal-footer">
+<button type="button" class="btn" data-bs-dismiss="modal">Close</button>
+</div>
+</div>
+</div>
+</div>
     </div>
       `
   })
@@ -104,6 +139,32 @@ async function getBasketItems(){
   let activeCustomer=data.find(item=>item.customerEmail===customer.userEmail)
   badge.innerHTML=activeCustomer.basketItems.length
   fillBasket(activeCustomer.basketItems)
-  // fillCustomerOrders(activeCustomer.orderedProducts)
+  fillCustomerOrders(activeCustomer.orderedProducts)
 }
 getBasketItems()
+let PRODUCTS_URL="http://localhost:3000/allProducts"
+
+let textarea="nmmnmn"
+function sendThought(e){
+textarea=e.value
+return textarea
+}
+
+let star=9
+function rate(num){
+return star=num
+}
+
+async  function sendReview(id){
+  const res= await axios(PRODUCTS_URL)
+  const data= await res.data
+  let ratedProd=data.find(item=>item.id===id)
+  console.log(ratedProd);
+  if(!ratedProd.userReview&&!ratedProd.productRating){
+    ratedProd.userReview=[]
+    ratedProd.productRating=[]
+  }
+  ratedProd.userReview.push(textarea)
+  ratedProd.productRating.push(star)
+  await axios.patch(`${PRODUCTS_URL}/${id}`, ratedProd)
+}
