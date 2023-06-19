2dab8d6e8f9000efe5f2a5d3648da7ddcc162559
Gunay HUmmatova <hummatovagunay02@gmail.com>
2023-06-19 10:20:25 +0400

    user can login back to his account

--- Clinique-clone-web-site/js/login.js
+++ Clinique-clone-web-site/js/login.js
@@ -5,21 +5,16 @@ let  form=document.querySelector('form')
 const USER_URL="http://localhost:4000/users"
 const CUSTOMER_URL="http://localhost:8000/customers"
 
-// async function checkUser(){
-//     const res= await axios(USER_URL)
-//     const data= await res.data
-//     console.log(data);
-//     const matchedUser = data.find(user => user.userEmail === userEmail.value && user.userPassword === userPassword.value);
-    
-// }
-// checkUser()
 
 
-form.addEventListener('submit', async function(){
+
+form.addEventListener('submit', async function(e){
+    e.preventDefault()
     const res= await axios(USER_URL)
     const data= await res.data
     console.log(data);
     const matchedUser = data.find(user => user.userEmail === userEmail.value && user.userPassword === userPassword.value);
+    console.log(matchedUser);
     if(matchedUser){
         window.location.href="user-account.html"
         localStorage.setItem("normalUser", JSON.stringify(matchedUser))
