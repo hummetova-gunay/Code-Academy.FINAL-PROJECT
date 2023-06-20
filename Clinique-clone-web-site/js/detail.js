0c929c42ad3103d44743b6e7f190b7d6fc61a676
Gunay HUmmatova <hummatovagunay02@gmail.com>
2023-06-20 12:38:38 +0400

    image section for detail of product added

---
+++ Clinique-clone-web-site/js/detail.js
@@ -0,0 +1,27 @@
+let menuIcon=document.querySelector('.fa-bars')
+let desktopHeader=document.querySelector('.header')
+let closeIcon=document.querySelector('.fa-x')
+let mobileHeading=document.querySelector('.mobile-header')
+menuIcon.addEventListener('click', function(){
+    mobileHeading.classList.add('show')
+    desktopHeader.classList.add('hide')
+})
+closeIcon.addEventListener('click', function(){
+    mobileHeading.classList.remove('show')
+    desktopHeader.classList.remove('hide')
+})
+
+localStorage.removeItem('isAdmin')
+
+let goAccount=document.querySelector('.goAccount')
+let signIn=document.querySelector('.signIn')
+let hasUser=JSON.parse(localStorage.getItem("normalUser"))
+console.log(hasUser);
+
+if(hasUser){
+    goAccount.style.display="block"
+    signIn.style.display="none"
+}else{
+    goAccount.style.display="none"
+    signIn.style.display="block"
+}
